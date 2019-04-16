02b3d56b28f8ec85a7aae64464d924dc99ae6eb3
scarlatt-ana <scarlattaparecida@gmail.com>
2019-04-16 11:33:06 -0300

    criar sistema de saque bancario

---
+++ script.js
@@ -0,0 +1,42 @@
+let senhaCadastrada = "nina123";
+let senhaEntrada = prompt ("Digite a sua senha começando com nina");
+let limiteDiario = 500;
+
+if (senhaCadastrada==senhaEntrada){
+    let saldo = 1000;
+    let vlrSaque = prompt ("Qual o valor do Saque");
+
+    if(vlrSaque<=saldo){
+        if(vlrSaque<=500){
+            if(vlrSaque==saldo){
+}
+    
+    alert ("Saque aprovado");    
+        }
+    }
+    
+    else{
+    alert ("Saldo incsuficiente");
+    }
+}
+       
+else{
+    alert ("Senha Incorreta");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
